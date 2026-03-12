b556dcf5c2c74c3d057ee5138bb6f5a363463381
Bikita Suwal <139045699+bikitasuwal@users.noreply.github.com>
2026-03-12 09:29:44 +0545

    Task 2 completed, Date:3/12/2026

--- home.py
+++ home.py
@@ -30,3 +30,16 @@ def calculate_bmi(weight, height):
     return bmi
 calculated_bmi = calculate_bmi(4, 1.75)
 print("Your BMI is:", round(calculated_bmi, 2))
+
+#Task 2: 
+def bmi_category(bmi):
+    if bmi< 18.5:
+        return "Underweight"
+    elif 18.5<=bmi<25:
+        return "Normal"
+    elif 25<=bmi<30:
+        return "Overweight"
+    else:
+      return "Obese"
+
+print("Category: ", bmi_category(calculated_bmi))
